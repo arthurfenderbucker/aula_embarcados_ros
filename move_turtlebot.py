4742c5a16cd499f8bc5b10ed62521421bdc915f3
ArthurBucker <Arthur.bucker@hotmail.com>
2019-10-21 08:55:15 -0300

    Update move_turtlebot.py

--- move_turtlebot.py
+++ move_turtlebot.py
@@ -29,24 +29,22 @@ def move():
     vel_msg.angular.y = 0
     vel_msg.angular.z = 0
 
-    while not rospy.is_shutdown():
+    #Setting the current time for distance calculus
+    t0 = rospy.Time.now().to_sec()
+    current_distance = 0
 
-        #Setting the current time for distance calculus
-        t0 = rospy.Time.now().to_sec()
-        current_distance = 0
-
-        #Loop to move the turtle in an specified distance
-        while(current_distance < distance):
-            #Publish the velocity
-            velocity_publisher.publish(vel_msg)
-            #Takes actual time to velocity calculus
-            t1=rospy.Time.now().to_sec()
-            #Calculates distancePoseStamped
-            current_distance= speed*(t1-t0)
-        #After the loop, stops the robot
-        vel_msg.linear.x = 0
-        #Force the robot to stop
+    #Loop to move the turtle in an specified distance
+    while(current_distance < distance):
+        #Publish the velocity
         velocity_publisher.publish(vel_msg)
+        #Takes actual time to velocity calculus
+        t1=rospy.Time.now().to_sec()
+        #Calculates distancePoseStamped
+        current_distance= speed*(t1-t0)
+    #After the loop, stops the robot
+    vel_msg.linear.x = 0
+    #Force the robot to stop
+    velocity_publisher.publish(vel_msg)
 
 if __name__ == '__main__':
     try:
